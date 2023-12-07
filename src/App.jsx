c154a0127d860ab8dea317124ac69b13a359d2dc
Adam Gnatowski <adam.gnatowski@performance-media.pl>
2023-12-07 16:25:33 +0100

    initial one

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Player from './components/Player.jsx';
+
+function App() {
+  
+
+  return (
+    <div id="game-container">
+      <ol id="players">
+        <Player name="Name 1" symbol="X" />
+        <Player name="Name 2" symbol="O" />
+      </ol>
+          BOARD
+    </div>
+
+  )
+}
+
+export default App
